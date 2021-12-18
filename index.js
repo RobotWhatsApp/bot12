7fd8f4f21746c4eed75a4b1b06ba0aee2254f342
RobotWhatsApp <93564595+RobotWhatsApp@users.noreply.github.com>
2021-12-18 19:25:28 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -194,9 +194,9 @@ autoketik = false
 autoregister = setting.user_register
 
 img = setting.img
-baper = 'SUB YT ZEEONE OFC'
-apiku = 'https://youtu.be/2rWbjrDIAyA'
-gc_wa_lu = 'https://chat.whatsapp.com/EU890BcXjyBDkNaUT5WmYV' //klo gk punya gc wa gk usah di ganti 👍
+baper = '© 𝐀𝐧𝐝𝐢𝐆𝐚𝐧𝐳'
+apiku = 'wa.me/6285725220424'
+gc_wa_lu = 'https://chat.whatsapp.com/EU89h0BcXjyBDkNaUT5WmYV' //klo gk punya gc wa gk usah di ganti 👍
 targetpc = setting.ownerNumberr
 owner = targetpc
 fake = setting.fake
